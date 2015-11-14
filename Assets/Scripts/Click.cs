49fdc624bd7385a7acbffaf990e26b16f5c2818c
ana.rdz27@hotmail.com <omarandstuff@gmail.com>
2015-11-14 13:55:10 -0600

    Disapear Balls

---
+++ Assets/Scripts/Click.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class Click : MonoBehaviour {
+
+	//private GameObject father;
+	public Move father;
+	void OnMouseDown (){
+		//GetComponent<MeshRenderer> ().enabled = false;
+		//transform.GetChild(0).GetComponent<MeshRenderer> ().enabled = false;
+		transform.parent.transform.position = new Vector3 (0.0f, -1.5f, 0.0f);
+		father.waiting = true;
+
+	}
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	public float speed = 0.5f;
+	public float top = 5.0f;
+	public float bottom = -1.0f;
+	public bool waiting = false;
+	public float waitingTime = 0.0f;
+	private float curret_Time;
+	private int flip = 1;
+	//public Rigidbody baloon;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!waiting) {
+			Vector3 direction = Vector3.up * speed * Time.deltaTime * flip;
+			transform.position += direction;
+			if (transform.position.y >= top) {
+				waiting = true;
+				flip = -1;
+			}
+			if (transform.position.y <= bottom) {
+				waiting = true;
+				flip = 1;
+			}
+			//transform.position += direction;
+		} else {
+			curret_Time += Time.deltaTime;
+			if (curret_Time >= waitingTime){
+				waiting = false;
+				curret_Time = 0.0f;
+			}
+
+		}
+
+	}
+
+
+}
