719fffe19803ececd4e75c123c598ac316aace7c
ana.rdz27@hotmail.com <omarandstuff@gmail.com>
2015-11-20 18:46:50 -0600

    finishing game, GFY stupid light

---
+++ Assets/Scripts/GoInstructions2.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoInstructions2 : MonoBehaviour {
+
+	void OnMouseDown () {
+		Application.LoadLevel (4);
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/GoMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoMenu : MonoBehaviour {
+
+	void OnMouseDown () {
+		Application.LoadLevel (0);
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
